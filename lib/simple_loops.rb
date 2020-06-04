73383655f236aff8f1b5f4d758b66492e53459fb
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-06-04 20:52:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,4 +1,16 @@
 # Write your methods here
 def loop_message_five_times (string)
-  puts string
+  5.times {puts string}
+end
+
+def loop_message_n_times (string, integer)
+  integer.times {puts string}
+end
+
+def output_array (array)
+  
+end
+
+def return_string_array(array)
+  
 end
